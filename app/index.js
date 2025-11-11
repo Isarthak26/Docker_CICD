a1e92c2d58ce0a4d52f42a783ee29bb21fa6c5fa
Sarthak <sarthakbordia10@gmail.com>
2025-11-11 20:20:33 +0530

    2nd commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js Docker CI/CD');
+});
+
+app.get('/healthz', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
